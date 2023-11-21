ae2b46c278e2795794284bb4ebd422d32c104ce1
sarker24 <sarker24@gmail.com>
2023-11-21 14:50:04 +0100

    Add data from api call in dropdown data

--- src/app/dropdown/page.tsx
+++ src/app/dropdown/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 
@@ -14,8 +15,23 @@ interface IFormInput {
 }
 
 export default function Dropdown() {
+  const [posts, setPosts] = useState([]);
   const { register, handleSubmit } = useForm<IFormInput>();
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  const dropdownOption = posts?.map((post: any) => {
+    return (
+      <option key={post.id} value={post.id}>
+        {post.title}
+      </option>
+    );
+  });
 
   return (
     <div className="flex items-center justify-center mt-11">
@@ -39,9 +55,7 @@ export default function Dropdown() {
             className="select select-bordered w-48 text-black"
             {...register("gender")}
           >
-            <option value="female">female</option>
-            <option value="male">male</option>
-            <option value="other">other</option>
+            {dropdownOption}
           </select>
         </div>
         <button className="btn btn-success my-2" type="submit">
