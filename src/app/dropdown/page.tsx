c12d89e2ea66b19793360579e2920ba2c3d75cf2
sarker24 <sarker24@gmail.com>
2023-11-21 12:50:14 +0100

    Impliment normal dropdown list

--- src/app/dropdown/page.tsx
+++ src/app/dropdown/page.tsx
@@ -1,3 +1,53 @@
-export default function dropdown() {
-  return <div>this is dropdown page</div>;
+"use client";
+import ReactDOM from "react-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+enum GenderEnum {
+  female = "female",
+  male = "male",
+  other = "other",
+}
+
+interface IFormInput {
+  name: string;
+  gender: GenderEnum;
+}
+
+export default function Dropdown() {
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  return (
+    <div className="flex items-center justify-center mt-11">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="name"
+            className="input input-bordered w-full max-w-xs"
+            {...register("name")}
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Select gender</span>
+          </label>
+          <select
+            className="select select-bordered w-48 text-black"
+            {...register("gender")}
+          >
+            <option value="female">female</option>
+            <option value="male">male</option>
+            <option value="other">other</option>
+          </select>
+        </div>
+        <button className="btn btn-success my-2" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
